37363a58aa36b94c58f88aaa85ff3deedb8838ba
efecaliskan1 <efecaliskan090@gmail.com>
2026-05-27 23:46:23 +0300

    Complete CityMap class

--- src/map/CityMap.java
+++ src/map/CityMap.java
@@ -29,3 +29,69 @@ public class CityMap {
                 col >= 0 &&
                 col < cols;
     }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getCols() {
+        return cols;
+    }
+
+    public List<Zone> getZones() {
+        List<Zone> zones = new ArrayList<>();
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                Cell cell = grid[r][c];
+
+                if (cell instanceof Zone) {
+                    zones.add((Zone) cell);
+                }
+            }
+        }
+
+        return zones;
+    }
+
+    public List<UtilityProvider> getUtilityProviders() {
+        List<UtilityProvider> providers = new ArrayList<>();
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                Cell cell = grid[r][c];
+
+                if (cell instanceof UtilityProvider) {
+                    providers.add((UtilityProvider) cell);
+                }
+            }
+        }
+
+        return providers;
+    }
+
+    public List<ServiceBuilding> getServiceBuildings() {
+        List<ServiceBuilding> services = new ArrayList<>();
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                Cell cell = grid[r][c];
+
+                if (cell instanceof ServiceBuilding) {
+                    services.add((ServiceBuilding) cell);
+                }
+            }
+        }
+
+        return services;
+    }
+
+    public void printSymbols() {
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                System.out.print(grid[r][c].getSymbol() + " ");
+            }
+            System.out.println();
+        }
+    }
+}
