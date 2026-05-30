9f213700c29bbfe2b5c57f24a5e16b6516cf71b0
parcalarCuma <cumaparcalar334@gmail.com>
2026-05-30 15:06:02 +0300

    Add  " ResourcePool " class for simulation resource tracking
    Added ResourcePool to store total Population, Goods and Lifestyle
    values produced during each simulation tick.
    Includes reset() method to clear resource totals before the next
    simulation step.

---
+++ src/simulation/ResourcePool.java
@@ -0,0 +1,13 @@
+package simulation;
+
+public class ResourcePool {
+    public int totalPopulation = 0;
+    public int totalGoods = 0;
+    public int totalLifestyle = 0;
+
+    public void reset() {
+        totalPopulation = 0;
+        totalGoods = 0;
+        totalLifestyle = 0;
+    }
+}
