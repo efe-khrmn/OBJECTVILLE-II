c6160b454f27491c91baf8b32ce92e6d00deb29f
nehirozsari <nehirozsari06@outlook.com>
2026-06-03 20:01:59 +0300

    Changing a parameter in Hospital.java

--- src/model/Hospital.java
+++ src/model/Hospital.java
@@ -2,6 +2,6 @@ package model;
 
 public class Hospital extends ServiceBuilding {
     public Hospital(int row, int col) {
-        super(row, col, 'D', ServiceType.HEALTH, 5);
+        super(row, col, 'D', ServiceType.HEALTH, 3);
     }
 }
